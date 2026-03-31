6b5a271ab7660e0bb748c04c7150124540e43e1a
Katharina Weinberger <kath.weinberger@icloud.com>
2026-03-31 19:37:17 +0200

    Drag & Drop

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,13 @@ extends Node2D
 var direction := Vector2.RIGHT
 var clickPending := false
 var clickPosition := Vector2.ZERO
+var hasFirstClick := false
+var isDragging := false
+var dragStartPosition := Vector2.ZERO
+var lastMousePosition := Vector2.ZERO
+var dragThreshold := 5.0
+var dragOffset := Vector2i.ZERO
+
 
 @onready var clickTimer := $ClickTimer
 @onready var duckNode := $Duck
@@ -25,22 +32,76 @@ func _ready() -> void:
 	
 	window.position = Vector2i(0, yPos)
 
+func _process(_delta: float) -> void:
+	if isDragging and not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		stopDrag()
+		isDragging = false
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		if clickTimer.time_left > 0:
-			clickTimer.stop()
-			clickPending = false
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		
+		if event.pressed:
+			clickPosition = event.position
+			dragStartPosition = event.position
+			lastMousePosition = event.position
 			
-			print("Du hast Probleme?\nWir haben die Lösung!\nIT-Support Hotline: 0664 / 123456789\n")
+			isDragging = false
+			
+			if hasFirstClick and clickTimer.time_left > 0:
+				clickTimer.stop()
+				hasFirstClick = false
+				clickPending = false
+				
+				print("Du hast Probleme?\nWir haben die Lösung!\nIT-Support Hotline: 0664 / 123456789\n")
+			
+			else:
+				hasFirstClick = true
+				clickPending = true
+				clickTimer.start()
 		
 		else:
-			clickPending = true
-			clickPosition = event.position
-			clickTimer.start()
+			if isDragging:
+				stopDrag()
+			
+			isDragging = false
+
+
+	elif event is InputEventMouseMotion:
+		var mousePos = event.position
+		
+		if clickPending and not isDragging:
+			if mousePos.distance_to(dragStartPosition) > dragThreshold:
+				isDragging = true
+				clickPending = false
+				hasFirstClick = false
+				clickTimer.stop()
+				
+				startDrag(mousePos)
+		
+		if isDragging:
+			lastMousePosition = mousePos
+			drag()
+
+func startDrag(pos: Vector2i):
+	var window = get_window()
+	dragOffset = window.position - Vector2i(DisplayServer.mouse_get_position())
+
+func drag():
+	var window = get_window()
+	var mouseGlobalPos = DisplayServer.mouse_get_position()
+	window.position = mouseGlobalPos + dragOffset
+
+func stopDrag():
+	var window = get_window()
+	var usableRect := DisplayServer.screen_get_usable_rect()
+	var yPos = usableRect.end.y - window.size.y
+	
+	var positionTween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	positionTween.tween_property(window, "position", Vector2i(window.position.x, yPos), abs(window.position.y - yPos) / 750.0)
 
 func _on_click_timer_timeout() -> void:
 	if clickPending:
+		hasFirstClick = false
 		clickPending = false
 	
 	duckNode.move(clickPosition.x < get_window().size.x / 2.0)
